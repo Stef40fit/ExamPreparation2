0a984f178dfa0e3915c0cb9ac580de0d257d5f0f
Stefan Ivanov <135533109+Stef40fit@users.noreply.github.com>
2023-11-01 19:01:45 +0200

    Add project files.

---
+++ TestApp.Tests/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using static NUnit.Framework.Constraints.Tolerance;
+
+namespace TestApp.Tests;
+
+public class LongestIncreasingSubsequenceTests
+{
+    [Test]
+    public void Test_GetLis_NullArray_ThrowsArgumentNullException()
+    {
+        //Arrange
+        int[]? arr = null;
+        //Act & Assert
+        Assert.That(()=> LongestIncreasingSubsequence.GetLis(arr), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetLis_EmptyArray_ReturnsEmptyString()
+    {
+        // Arrange
+        int[] arr = { };
+        //Act 
+        string output = LongestIncreasingSubsequence.GetLis(arr);
+        // Assert
+        Assert.IsEmpty(output);
+    }
+        [Test]
+    public void Test_GetLis_SingleElementArray_ReturnsElement()
+    {
+        int[] arr = {1};
+        //Act 
+        string output = LongestIncreasingSubsequence.GetLis(arr);
+        // Assert
+        Assert.That(output, Is.EqualTo("1"));
+    }
+
+    [Test]
+    public void Test_GetLis_UnsortedArray_ReturnsLongestIncreasingSubsequence()
+    {
+        int[] arr = { 12, 3, 2, 4 };
+        //Act 
+        string output = LongestIncreasingSubsequence.GetLis(arr);
+        // Assert
+        Assert.That(output, Is.EqualTo("3 4"));
+    }
+
+    [Test]
+    public void Test_GetLis_SortedArray_ReturnsItself()
+    {
+        int[] arr = { 1, 2, 3, 4};
+        //Act 
+        string output = LongestIncreasingSubsequence.GetLis(arr);
+        // Assert
+        Assert.That(output, Is.EqualTo("1 2 3 4"));
+    }
+}
